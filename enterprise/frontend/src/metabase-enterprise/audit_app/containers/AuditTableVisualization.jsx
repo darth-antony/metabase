cbcf095d7fd64e99a778673798c95f298a946c67
Alexander Lesnenko <alxnddr@users.noreply.github.com>
2021-09-15 16:00:29 +0200

    fixes

--- enterprise/frontend/src/metabase-enterprise/audit_app/containers/AuditTableVisualization.jsx
+++ enterprise/frontend/src/metabase-enterprise/audit_app/containers/AuditTableVisualization.jsx
@@ -14,10 +14,9 @@ import Table from "metabase/visualizations/visualizations/Table";
 import EmptyState from "metabase/components/EmptyState";
 import Icon from "metabase/components/Icon";
 import { RemoveRowButton } from "./AuditTableVisualization.styled";
+import { getRowValuesByColumns, getColumnName } from "../lib/util";
 
 import NoResults from "assets/img/no_results.svg";
-
-const getColumnName = column => column.remapped_to || column.name;
 
 const propTypes = {
   series: PropTypes.array,
@@ -57,15 +56,9 @@ export default class AuditTableVisualization extends React.Component {
     });
   };
 
-  handleRemoveRowClick = (row, cols, columnIndexes) => {
-    const rowMap = columnIndexes.reduce((acc, index) => {
-      const columnName = getColumnName(cols[index]);
-      return {
-        ...acc,
-        [columnName]: row[index],
-      };
-    }, {});
-    this.props.onRemoveRow(rowMap);
+  handleRemoveRowClick = (row, cols) => {
+    const rowData = getRowValuesByColumns(row, cols);
+    this.props.onRemoveRow(rowData);
   };
 
   render() {
@@ -84,7 +77,6 @@ export default class AuditTableVisualization extends React.Component {
     } = this.props;
 
     const canRemoveRows = !!onRemoveRow;
-
     const columnIndexes = settings["table.columns"]
       .filter(({ enabled }) => enabled)
       .map(({ name }) => _.findIndex(cols, col => col.name === name));
@@ -167,9 +159,7 @@ export default class AuditTableVisualization extends React.Component {
               {canRemoveRows && (
                 <td>
                   <RemoveRowButton
-                    onClick={() =>
-                      this.handleRemoveRowClick(row, cols, columnIndexes)
-                    }
+                    onClick={() => this.handleRemoveRowClick(row, cols)}
                   >
                     <Icon name="close" color="text-light" />
                   </RemoveRowButton>
